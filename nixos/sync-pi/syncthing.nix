2f3483c7bac6d06dd5fa12765cfd8202c09f6aa2
John Otwell <john.otwell@proton.me>
2023-07-30 19:39:31 -0400

    secrets management

--- nixos/sync-pi/syncthing.nix
+++ nixos/sync-pi/syncthing.nix
@@ -9,11 +9,6 @@
       # overrideDevices = true;
       # overrideFolders = true;
       guiAddress = "0.0.0.0:8384";
-      extraOptions.gui = {
-        user = "john";
-        password =
-          "sha256:2eabb82418d1665d6adfa8b9b60ddb752c000f95470436fdb66b0442255b7f66";
-      };
       # devices = {
       # "pixel" = {
       # id =
