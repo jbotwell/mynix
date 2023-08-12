f73266673920958109726dc1759cfbc1569d60e6
John Otwell <john.otwell@proton.me>
2023-08-12 14:55:35 -0400

    transmission access control, local network

--- nixos/media-pi/transmission.nix
+++ nixos/media-pi/transmission.nix
@@ -3,9 +3,17 @@
 {
   services.transmission = {
     enable = true;
-    home = "/home/john/";
+    user = "john";
+    home = "/home/john";
     settings = {
       watch-dir-enabled = true; # should be <home>/watchdir by default
+      rpc-bind-address = "0.0.0.0";
+      rpc-whitelist = "192.168.1.*";
+      rpc-host-whitelist = "media-pi";
     };
+    openFirewall = true;
+    openRPCPort = true;
   };
+
+  # networking.firewall.allowedTCPPorts = [ 9091 53741 ];
 }
