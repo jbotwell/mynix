47bd64719b4e2e1279565862052625da0bea1111
John Otwell <john.otwell@protonmail.com>
2023-09-23 12:24:11 -0400

    add back devices

--- nixos/sync-pi/syncthing-lead.nix
+++ nixos/sync-pi/syncthing-lead.nix
@@ -10,28 +10,28 @@
       overrideDevices = true;
       overrideFolders = true;
       guiAddress = "0.0.0.0:8384";
-      # devices = {
-      #   "pixel" = {
-      #     id =
-      #       "QU3WGRO-2GEGWKI-5SGDSSL-EU564OU-36MCRBC-CPX04RJ-0TMWMBX-EAZGTQV";
-      #   };
-      #   "fw" = {
-      #     id =
-      #       "OD3R4RA-IAA4SXH-PZ3G2MP-5WC7OUS-ZPSAPYE-EEC4WBZ-46IJB2O-5GIRPQZ";
-      #   };
-      #   "media-pi" = {
-      #     id =
-      #       "OD3R4RA-IAA4SXH-PZ3G2MP-5WC7OUS-ZPSAPYE-EEC4WBZ-46IJB2O-5GIRPQZ";
-      #   };
-      # };
+      devices = {
+        "pixel" = {
+          id =
+            "QU3WGRO-2GEGWKI-5SGDSSL-EU564OU-36MCRBC-CPX04RJ-0TMWMBX-EAZGTQV";
+        };
+        "fw" = {
+          id =
+            "547RYKF-VZCNPLZ-F5TCQY7-LUOLRLK-KM2KUBU-4RJNIMB-P7KBHFG-7BNUSQM";
+        };
+        #   "media-pi" = {
+        #     id =
+        #       "OD3R4RA-IAA4SXH-PZ3G2MP-5WC7OUS-ZPSAPYE-EEC4WBZ-46IJB2O-5GIRPQZ";
+        #   };
+      };
       folders = {
         "sync" = {
           path = "/home/john/sync";
-          devices = [ ];
+          devices = [ "pixel" "fw" ];
         };
         "org" = {
           path = "/home/john/org";
-          devices = [ ];
+          devices = [ "pixel" "fw" ];
         };
       };
     };
