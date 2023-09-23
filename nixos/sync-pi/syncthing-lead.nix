660eb97b17d2ef49f0f6c89428ccebdbf9ab5b10
John Otwell <john.otwell@protonmail.com>
2023-09-23 12:14:16 -0400

    comment old st ids

--- nixos/sync-pi/syncthing-lead.nix
+++ nixos/sync-pi/syncthing-lead.nix
@@ -10,30 +10,30 @@
       overrideDevices = true;
       overrideFolders = true;
       guiAddress = "0.0.0.0:8384";
-      devices = {
-        # "pixel" = {
-        #   id =
-        #     "QU3WGRO-2GEGWKI-5SGDSSL-EU564OU-36MCRBC-CPX04RJ-0TMWMBX-EAZGTQV";
-        # };
-        # "fw" = {
-        #   id =
-        #     "OD3R4RA-IAA4SXH-PZ3G2MP-5WC7OUS-ZPSAPYE-EEC4WBZ-46IJB2O-5GIRPQZ";
-        # };
-        # "media-pi" = {
-        #   id =
-        #     "OD3R4RA-IAA4SXH-PZ3G2MP-5WC7OUS-ZPSAPYE-EEC4WBZ-46IJB2O-5GIRPQZ";
-        # };
-      };
-      folders = {
-        "sync" = {
-          path = "/home/john/sync";
-          devices = [ "pixel" "fw" "media-pi" ];
-        };
-        "org" = {
-          path = "/home/john/org";
-          devices = [ "pixel" "fw" "media-pi" ];
-        };
-      };
+      # devices = {
+      #   "pixel" = {
+      #     id =
+      #       "QU3WGRO-2GEGWKI-5SGDSSL-EU564OU-36MCRBC-CPX04RJ-0TMWMBX-EAZGTQV";
+      #   };
+      #   "fw" = {
+      #     id =
+      #       "OD3R4RA-IAA4SXH-PZ3G2MP-5WC7OUS-ZPSAPYE-EEC4WBZ-46IJB2O-5GIRPQZ";
+      #   };
+      #   "media-pi" = {
+      #     id =
+      #       "OD3R4RA-IAA4SXH-PZ3G2MP-5WC7OUS-ZPSAPYE-EEC4WBZ-46IJB2O-5GIRPQZ";
+      #   };
+      # };
+      # folders = {
+      #   "sync" = {
+      #     path = "/home/john/sync";
+      #     devices = [ "pixel" "fw" "media-pi" ];
+      #   };
+      #   "org" = {
+      #     path = "/home/john/org";
+      #     devices = [ "pixel" "fw" "media-pi" ];
+      #   };
+      # };
     };
   };
 }
