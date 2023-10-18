d937ac4921fca005806af0e94b68feb08d1a0059
John Otwell <john.otwell@protonmail.com>
2023-10-18 12:23:47 -0400

    delta/git options

--- home-manager/programs/git.nix
+++ home-manager/programs/git.nix
@@ -4,22 +4,16 @@
   programs.git = {
     enable = true;
     delta.enable = true;
-    delta.options = {
-      features = {
-        side_by_side = true;
-        decorations = true;
-      };
-      syntax_theme = "Dracula";
-    };
     userName = "John Otwell";
     userEmail = "john.otwell@protonmail.com";
     extraConfig = {
-      interactive.diffFilter = "delta --color-only";
       merge.conflictStyle = "diff3";
       diff.colorMoved = "default";
       mergetool.vimdiff = true;
       pull.rebase = false;
       init.defaultBranch = "main";
+      delta.side-by-side = true;
+      delta.line-numbers = true;
     };
   };
 }
