535b57f1b460f654744488044dab8783fdb8cf6d
John Otwell <john.otwell@protonmail.com>
2023-11-20 15:23:18 -0500

    darwin pkgs

--- flake.nix
+++ flake.nix
@@ -6,6 +6,10 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
     unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
+    # darwin
+    darwin.url = "github:NixOS/nixpkgs/nixpkgs-23.05-darwin";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
     # home-manager
     home-manager = {
       url = "github:nix-community/home-manager/release-23.05";
@@ -15,9 +19,19 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "unstable";
     };
+
+    # darwin home-manager
+    # darwin-home-manager = {
+    #   url = "github:nix-community/home-manager/release-23.05";
+    #   inputs.nixpkgs.follows = "darwin";
+    # };
+    # darwin-home-unstable = {
+    #   url = "github:nix-community/home-manager";
+    #   inputs.nixpkgs.follows = "nixpkgs-unstable";
+    # };
   };
 
-  outputs = { nixpkgs, home-manager, ... }@inputs: {
+  outputs = { nixpkgs, home-manager, darwin, ... }@inputs: {
     # NixOS configuration entrypoint
     nixosConfigurations = let
       mkSystem = inputs: additionalModules:
@@ -35,7 +49,6 @@
       fw = mkSystem inputs [ ./nixos/fw/configuration.nix ];
       sync-pi = mkSystem inputs [ ./nixos/sync-pi/configuration.nix ];
       media-pi = mkSystem inputs [ ./nixos/media-pi/configuration.nix ];
-      wsl = mkSystem inputs [ ./nixos/wsl/configuration.nix ];
     };
 
     # Standalone home-manager configuration entrypoint
@@ -51,14 +64,15 @@
         modules = [ ./home-manager/john.nix ];
       };
       "john@work" = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs {
+        pkgs = import darwin {
           system = "aarch64-darwin";
           config.allowUnfree = true;
         }; # Home-manager requires 'pkgs' instance
         extraSpecialArgs = {
-          inherit inputs;
+          inputs.nixpkgs = inputs.darwin;
+          inputs.unstable = inputs.nixpkgs-unstable;
         }; # Pass flake inputs to our config
-        modules = [ ./home-manager/john.nix ];
+        modules = [ ./home-manager/john-work.nix ];
       };
     };
   };
