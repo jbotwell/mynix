6da0e443a29544cb1711c5080646ca6af7d17381
John Otwell <john.otwell@protonmail.com>
2023-11-19 19:37:40 -0500

    bash-it from my git

--- home-manager/programs/bash.nix
+++ home-manager/programs/bash.nix
@@ -1,6 +1,12 @@
 { pkgs, ... }:
-
-{
+let
+  myBashIt = pkgs.fetchFromGitHub {
+    owner = "jbotwell";
+    repo = "my_bash_it";
+    rev = "main";
+    sha256 = "02g10fg9dklny7g930hr4gk2c9z0zqa80v45c90sfd7f453khk1x";
+  };
+in {
   programs.bash.enable = true;
   programs.bash.initExtra = ''
     export PATH=$PATH:/home/john/.npm/bin:/home/john/.dotnet/tools
@@ -9,6 +15,9 @@
 
     alias et="emacsclient -nw"
     alias xc='xclip -sel clip'
+    ch() {
+      tmux split-window -h bash -c "curl cht.sh/$1 | less"
+    }
     eval "$(thefuck --alias)"
 
     set -o vi
@@ -19,4 +28,5 @@
     fi
 
     source "$BASH_IT"/bash_it.sh'';
+  home.file.".bash_it".source = myBashIt;
 }
