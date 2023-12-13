a16390d6e23651a8d87a4912960f6e688acba8c2
John Otwell <john.otwell@protonmail.com>
2023-12-13 10:54:05 -0500

    bash files; edit ch

--- home-manager/programs/bash.nix
+++ home-manager/programs/bash.nix
@@ -19,9 +19,19 @@ in {
     alias bashbare='bash --noprofile --norc'
     alias gsv='git status -v'
 
+    # cheatsheets
+    # usage: `ch git~worktree` for tools`
+    # usage: `ch go/:learn` for languages`
+    # usage: `ch go/reverse+a+list` for languages`
     ch() {
       tmux split-window -h bash -c "curl cht.sh/$1 | less -r"
     }
+
+    # fullscreen of the above
+    chf() {
+      curl cht.sh/$1~$2 | less -r
+    }
+
     eval "$(thefuck --alias)"
 
     set -o vi
