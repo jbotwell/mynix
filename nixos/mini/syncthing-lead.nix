9ddfebcb2a0afbb0549f85c72108c5af2356a812
John Otwell <john.otwell@proton.me>
2024-02-06 00:07:56 -0500

    syncthing twiddling

--- nixos/mini/syncthing-lead.nix
+++ nixos/mini/syncthing-lead.nix
@@ -1,6 +1,9 @@
 { ... }:
 
 {
+  networking.firewall.allowedTCPPorts = [ 8384 22000 ];
+  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
+
   services = {
     syncthing = {
       enable = true;
