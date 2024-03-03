82b9d10e0b570f080870077d2cc280cf031206af
John Otwell <john.otwell@proton.me>
2024-03-03 00:52:57 -0500

    syncthing changes

--- modules/nixos/syncthing-lead.nix
+++ modules/nixos/syncthing-lead.nix
@@ -1,46 +1,61 @@
 { ... }:
+let
+  dataDir = "/syncthing";
+  configDir = "${dataDir}/config";
+  sync = "${dataDir}/sync";
+  org = "${dataDir}/org";
+in {
+  systemd.tmpfiles.rules = [
+    "d ${dataDir} 0755 john users"
+    "d ${configDir} 0755 john users"
+    "d ${sync} 0755 john users"
+    "d ${org} 0755 john users"
+  ];
 
-{
   networking.firewall.allowedTCPPorts = [ 8384 22000 ];
   networking.firewall.allowedUDPPorts = [ 22000 21027 ];
 
   services = {
     syncthing = {
+      inherit dataDir configDir;
       enable = true;
       user = "john";
-      dataDir = "/home/john";
-      configDir = "/home/john/.config/syncthing";
       overrideDevices = true;
       overrideFolders = true;
       guiAddress = "0.0.0.0:8384";
       settings = {
         devices = {
-          "pixel" = {
-            id =
-              "QU3WGRO-2GEGWKI-5SGDSSL-EU564OU-36MCRBC-CPX04RJ-0TMWMBX-EAZGTQV";
-          };
           "fw" = {
             id =
               "547RYKF-VZCNPLZ-F5TCQY7-LUOLRLK-KM2KUBU-4RJNIMB-P7KBHFG-7BNUSQM";
+          };
+          "pixel" = {
+            id =
+              "QU3WGRO-2GEGWKI-5SGDSSL-EU564OU-36MCRBC-CPXO4RJ-OTMWMBX-EAZGTQV";
+          };
+          "samsung-tab" = {
+            id =
+              "U4BLU3O-VGCST4Z-YFJ6NLY-UA2MT2S-AI3TAAR-KHU54VO-EBLYPDQ-EQ26LAY";
           };
           "spg" = {
             id =
               "E53ZNC4-KRSRXEY-UMUFKXQ-FR7I7KC-SOVJ7K2-3CDEUMG-HJRG477-DCDCOQU";
           };
-          "samsung-tab" = {
-            id =
-              "U4BLU30-VGCST4Z-YFJ6NLY-UA2MT2S-AI3TAAR-KHU54V0-EBLYPDQ-EQ26LAY";
-          };
         };
         folders = {
           "sync" = {
-            path = "/home/john/sync";
+            path = sync;
             devices = [ "pixel" "fw" "samsung-tab" "spg" ];
           };
           "org" = {
-            path = "/home/john/org";
+            path = org;
             devices = [ "pixel" "fw" "samsung-tab" "spg" ];
           };
+        };
+        gui = {
+          user = "john.otwell";
+          # bcrypt
+          password = "$2y$10$cKvo2rJLj5vzHdgPpEhrtu4gmdUCCZ0LvahBeKGA/b8oljK6cyHHO";
         };
       };
     };
