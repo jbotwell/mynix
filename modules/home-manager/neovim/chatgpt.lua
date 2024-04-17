93fca16e6f8210dd62a2fd943a9f545bb67a0844
John Otwell <john.otwell@protonmail.com>
2024-04-17 00:18:52 -0400

    chatgpt minor tweaks

--- modules/home-manager/neovim/chatgpt.lua
+++ modules/home-manager/neovim/chatgpt.lua
@@ -2,7 +2,7 @@ require("chatgpt").setup({
     api_host_cmd = "echo https://openrouter.ai/api",
     api_key_cmd = "pass show openrouter",
     openai_params = {
-        model ="mistralai/mixtral-8x7b-instruct",
+        model = "mistralai/mixtral-8x7b-instruct",
         frequency_penalty = 0,
         presence_penalty = 0,
         max_tokens = 1000,
@@ -18,5 +18,12 @@ require("chatgpt").setup({
         top_p = 1,
         n = 1
     },
-    chat = {keymaps = {cycle_windows = "<C-b>"}}
+    chat = {
+        keymaps = {cycle_windows = "<C-b>"},
+        welcome_message = [[
+
+	      do the thing.
+
+	]]
+    }
 })
