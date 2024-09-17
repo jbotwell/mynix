f3ecc90de5411ae884b25b2f360ab41ea1d6b1ee
John Otwell <john.otwell@proton.me>
2024-09-17 17:14:04 -0400

    ec2

--- hosts/awnix/configuration.nix
+++ hosts/awnix/configuration.nix
@@ -1,10 +1,11 @@
 {pkgs, ...}: {
   imports = [
-    ./hardware-configuration.nix
+    # ./hardware-configuration.nix
     ../shared/nixos-server.nix
-    ../../modules/nixos/syncthing-lead.nix
-    ../../modules/nixos/lnd.nix
-    ../../modules/nixos/rss.nix
+    ../shared/john-server.nix
+    # ../../modules/nixos/syncthing-lead.nix
+    # ../../modules/nixos/lnd.nix
+    # ../../modules/nixos/rss.nix
     "${pkgs}/nixos/modules/virtualisation/amazon-image.nix"
   ];
 
