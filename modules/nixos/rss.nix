518f57ad98aaec9c6fb8fc54fe9a90161d12d903
John Otwell <john.otwell@proton.me>
2024-09-18 01:55:16 -0400

    locally working rss

--- modules/nixos/rss.nix
+++ modules/nixos/rss.nix
@@ -1,39 +1,45 @@
-{ config, pkgs, ... }:
-
-{
+{pkgs, ...}: let
+  ttrss = "ttrss";
+in {
   services.tt-rss = {
     enable = true;
-    virtualHost = "tt-rss.example.com";
-    selfUrlPath = "https://tt-rss.example.com";
+    virtualHost = "rss.otwell.dev";
+    selfUrlPath = "https://rss.otwell.dev";
     database = {
       type = "pgsql";
       host = "localhost";
-      name = "ttrss";
-      user = "ttrss";
-      password = "$(cat /run/secrets/ttrss_password)";
+      name = ttrss;
+      user = ttrss;
+      createLocally = false;
     };
   };
 
   services.postgresql = {
     enable = true;
-    ensureDatabases = [ "ttrss" ];
+    ensureDatabases = [ttrss];
     ensureUsers = [
       {
-        name = "ttrss";
-        ensurePermissions."DATABASE ttrss" = "ALL PRIVILEGES";
+        name = ttrss;
+        ensureDBOwnership = true;
       }
     ];
+    authentication = pkgs.lib.mkOverride 10 ''
+      local all all trust
+      host all all 127.0.0.1/32 trust
+      host all all ::1/128 trust
+    '';
   };
 
   services.nginx = {
     enable = true;
-    virtualHosts."tt-rss.example.com" = {
+    virtualHosts."rss.otwell.dev" = {
       forceSSL = true;
       enableACME = true;
     };
   };
 
+  networking.firewall.allowedTCPPorts = [80 443];
+
   security.acme.acceptTerms = true;
-  security.acme.email = "your_email@example.com";
-}
+  security.acme.defaults.email = "john.otwell@proton.me";
 }
