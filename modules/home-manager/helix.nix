57c60523f62fcda25295fa26da18a5e5a091ccb9
John Otwell <john.otwell@proton.me>
2024-09-24 02:02:25 -0400

    helix, wait on snippets

--- modules/home-manager/helix.nix
+++ modules/home-manager/helix.nix
@@ -4,6 +4,9 @@
     # helix-specific lsp's
     helix-gpt
     markdown-oxide
+
+    # other lsp's
+    marksman
   ];
 
   programs.helix.settings = {
@@ -20,23 +23,13 @@
     language = [
       {
         name = "markdown";
-        # language-servers = [ "marksman", "markdown-oxide", "hx-lsp" ];
-        language-servers = ["hx-lsp"];
+        language-servers = ["marksman" "markdown-oxide"];
       }
     ];
-
-    language-server.hx-lsp = with pkgs; {
-      command = "${hx-lsp}/bin/hx-lsp";
-    };
 
     language-server.typescript-language-server = with pkgs.nodePackages; {
       command = "${typescript-language-server}/bin/typescript-language-server";
       args = ["--stdio" "--tsserver-path=${typescript}/lib/node_modules/typescript/lib"];
     };
-  };
-
-  home.file.".config/helix/snippets" = {
-    source = ./snippets;
-    recursive = true;
   };
 }
