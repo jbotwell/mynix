de376d55dbc6f3feb864cf88bdd7ca3ba33e34c3
John Otwell <john.otwell@proton.me>
2024-09-24 21:42:08 -0400

    moved lsp-ai

--- modules/home-manager/helix.nix
+++ modules/home-manager/helix.nix
@@ -1,7 +1,6 @@
 {
   pkgs,
   inputs,
-  lib,
   ...
 }: let
   naersk = pkgs.callPackage inputs.naersk {};
@@ -16,14 +15,12 @@ in {
   programs.helix.extraPackages = with pkgs; [
     # helix-specific lsp's
     lsp-ai
-    helix-gpt
-    markdown-oxide
 
-    # other lsp's
+    # lsp's
+    # md
+    markdown-oxide
     marksman
   ];
-
-  home.file.".config/helix/languages.toml".source = ./languages.toml;
 
   programs.helix.settings = {
     editor = {
@@ -36,47 +33,29 @@ in {
 
     keys.normal = {
       C-b = "increment";
+      g = {q = ":reflow";};
     };
   };
 
-  # programs.helix.languages = {
-  #   language = [
-  #     {
-  #       name = "markdown";
-  #       language-servers = [
-  #         "marksman"
-  #         "markdown-oxide"
-  #         "lsp-ai"
-  #       ];
-  #     }
-  #   ];
+  programs.helix.languages = {
+    language = [
+      {
+        name = "markdown";
+        language-servers = [
+          "marksman"
+          "markdown-oxide"
+          "lsp-ai"
+        ];
+      }
+    ];
 
-  #   language-server.lsp-ai = {
-  #     command = "${lsp-ai}/bin/lsp-ai";
-  #     config.memory = {
-  #       file_store = {};
-  #     };
-  #     config.models.csonnet = {
-  #       type = "anthropic";
-  #       chat_endpoint = "https://api.anthropic.com/v1/messages";
-  #       model = "claude-3-5-sonnet-20240620";
-  #       auth_token_env_var_name = "ANTHROPIC_API_KEY";
-  #     };
-  #     config.chat = {
-  #       trigger = "!C";
-  #       action_display_name = "Chat";
-  #       model = "csonnet";
-  #       parameters = {
-  #         max_context = 4096;
-  #         max_tokens = 1024;
-  #         system = "You are a code assistant chatbot. The user will ask you for assistance coding and you will do you best to answer succinctly and accurately";
-  #       };
-  #     };
-  #   };
+    language-server = {
+      lsp-ai = import ./lsp-ai.nix;
 
-  #   language-server.typescript-language-server = with pkgs.nodePackages; {
-  #     command = "${typescript-language-server}/bin/typescript-language-server";
-  #     args = ["--stdio" "--tsserver-path=${typescript}/lib/node_modules/typescript/lib"];
-  #   };
-  # };
+      typescript-language-server = with pkgs.nodePackages; {
+        command = "${typescript-language-server}/bin/typescript-language-server";
+        args = ["--stdio" "--tsserver-path=${typescript}/lib/node_modules/typescript/lib"];
+      };
+    };
+  };
 }
