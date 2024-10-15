81a4222e8730c07009922b50be4b4d553401b2c7
John Otwell <john.otwell@proton.me>
2024-10-15 11:37:03 -0400

    formatter

--- flake.nix
+++ flake.nix
@@ -28,82 +28,92 @@
     };
   };
 
-  outputs = {
-    nixpkgs,
-    home-manager,
-    stylix,
-    nix-bitcoin,
-    ...
-  } @ inputs: {
-    nixosConfigurations = let
-      bitcoinConfig = {
-        systemd.tmpfiles.rules = [
-          "d /mnt/data/bitcoind 0770 bitcoin bitcoin"
-        ];
-        services.bitcoind.dataDir = "/mnt/data/bitcoind";
+  outputs =
+    {
+      nixpkgs,
+      home-manager,
+      stylix,
+      nix-bitcoin,
+      ...
+    }@inputs:
+    {
+      nixosConfigurations =
+        let
+          bitcoinConfig = {
+            systemd.tmpfiles.rules = [
+              "d /mnt/data/bitcoind 0770 bitcoin bitcoin"
+            ];
+            services.bitcoind.dataDir = "/mnt/data/bitcoind";
 
-        nix-bitcoin.generateSecrets = true;
+            nix-bitcoin.generateSecrets = true;
 
-        services.bitcoind.enable = true;
-        services.electrs.enable = true;
-        services.lnd.enable = true;
-        services.rtl.enable = true;
-        services.rtl.nodes.lnd.enable = true;
+            services.bitcoind.enable = true;
+            services.electrs.enable = true;
+            services.lnd.enable = true;
+            services.rtl.enable = true;
+            services.rtl.nodes.lnd.enable = true;
 
-        nix-bitcoin.operator = {
-          enable = true;
-          name = "john";
-        };
-      };
-      specialArgs = {inherit inputs;};
-      xtxHome = ./hosts/xtx/john.nix;
-      xtxNixos = ./hosts/xtx/configuration.nix;
-      fwHome = ./hosts/fw/john.nix;
-      fwNixos = ./hosts/fw/configuration.nix;
-      miniHome = ./hosts/mini/john.nix;
-      miniNixos = ./hosts/mini/configuration.nix;
-      mkSystem = hm-module: otherModules:
-        nixpkgs.lib.nixosSystem {
-          inherit specialArgs;
-          modules =
-            [
-              home-manager.nixosModules.home-manager
-              {
-                home-manager.useGlobalPkgs = true;
-                home-manager.useUserPackages = true;
-                home-manager.extraSpecialArgs = specialArgs;
-                home-manager.users.john = import hm-module;
-              }
-            ]
-            ++ otherModules;
-        };
-    in {
-      fw = mkSystem fwHome [fwNixos stylix.nixosModules.stylix];
-      mini = mkSystem miniHome [
-        miniNixos
-        nix-bitcoin.nixosModules.default
-        bitcoinConfig
-      ];
-      xtx = mkSystem xtxHome [
-        xtxNixos
-        stylix.nixosModules.stylix
-      ];
-    };
-
-    homeConfigurations = {
-      "john@work" = home-manager.lib.homeManagerConfiguration {
-        pkgs = import nixpkgs {
-          system = "aarch64-darwin";
-          config.allowUnfree = true;
-          config.permittedInsecurePackages = [
-            "electron-25.9.0"
+            nix-bitcoin.operator = {
+              enable = true;
+              name = "john";
+            };
+          };
+          specialArgs = {
+            inherit inputs;
+          };
+          xtxHome = ./hosts/xtx/john.nix;
+          xtxNixos = ./hosts/xtx/configuration.nix;
+          fwHome = ./hosts/fw/john.nix;
+          fwNixos = ./hosts/fw/configuration.nix;
+          miniHome = ./hosts/mini/john.nix;
+          miniNixos = ./hosts/mini/configuration.nix;
+          mkSystem =
+            hm-module: otherModules:
+            nixpkgs.lib.nixosSystem {
+              inherit specialArgs;
+              modules = [
+                home-manager.nixosModules.home-manager
+                {
+                  home-manager.useGlobalPkgs = true;
+                  home-manager.useUserPackages = true;
+                  home-manager.extraSpecialArgs = specialArgs;
+                  home-manager.users.john = import hm-module;
+                }
+              ] ++ otherModules;
+            };
+        in
+        {
+          fw = mkSystem fwHome [
+            fwNixos
+            stylix.nixosModules.stylix
+          ];
+          mini = mkSystem miniHome [
+            miniNixos
+            nix-bitcoin.nixosModules.default
+            bitcoinConfig
+          ];
+          xtx = mkSystem xtxHome [
+            xtxNixos
+            stylix.nixosModules.stylix
           ];
         };
-        extraSpecialArgs = {inherit inputs;};
-        modules = [./hosts/work/john.nix];
-      };
-    };
 
-    formatter = nixpkgs.alejandra;
-  };
+      homeConfigurations = {
+        "john@work" = home-manager.lib.homeManagerConfiguration {
+          pkgs = import nixpkgs {
+            system = "aarch64-darwin";
+            config.allowUnfree = true;
+            config.permittedInsecurePackages = [
+              "electron-25.9.0"
+            ];
+          };
+          extraSpecialArgs = {
+            inherit inputs;
+          };
+          modules = [ ./hosts/work/john.nix ];
+        };
+      };
+
+      formatter = nixpkgs.nixfmt-rfc-style;
+    };
 }
